3f4a11386dd8c884206d7d36c30b99adc46609b9
lenghia100703 <21020366@vnu.edu.vn>
2022-07-12 00:22:02 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+
+import { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [name, setName] = useState('')
+  const [code, setCode] = useState('')
+  const [products, setProducts] = useState([])
+  
+  const nameRef = useRef()
+
+  const handleClick = () => {
+    setProducts([...products, {
+      name,
+      code: parseInt(code)
+    }])
+    setName('')
+    setCode('')
+    nameRef.current.focus()
+  }
+
+  const handleDelete = (index) => {
+    setProducts((prev) => {
+      const deleteProducts = [...prev];
+      deleteProducts.splice(index, 1);
+      return deleteProducts;
+    });
+  };
+  
+  
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input 
+        type="text" 
+        value={name}
+        placeholder="Enter name" 
+        ref={nameRef}
+        onChange={e => setName(e.target.value)}
+      />
+      <br />
+      <input 
+        type="text" 
+        value={code}
+        placeholder="Enter student code"
+        onChange={e => setCode(e.target.value)}
+      />
+      <br />
+      <button onClick={handleClick}>Add</button>
+      <br />
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            {product.name} - {product.code}
+            <button 
+              style={{margin: 8}}
+              onClick={() => handleDelete(index)}
+            >
+              {"x" || ""}
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
